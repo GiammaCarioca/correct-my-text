109c389c73bbe406178e97ba15c3feed844659e0
Thiago Giammattey <giammattey@gmail.com>
2020-01-12 14:11:58 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+;(function() {
+	'use strict'
+
+	/**
+	 * String.prototype.trim() polyfill
+	 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim#Polyfill
+	 */
+	if (!String.prototype.trim) {
+		String.prototype.trim = function() {
+			return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '')
+		}
+	}
+
+	const text = document.querySelector('#text')
+	const count = document.querySelector('#count')
+	const maxWords = 5
+
+	function checkMaxWords(words) {
+		if (!words) return
+
+		if (words > maxWords) {
+			text.classList.add('exceeded')
+			count.classList.add('exceeded')
+		} else {
+			text.classList.remove('exceeded')
+			count.classList.remove('exceeded')
+		}
+	}
+
+	function updateCount() {
+		const chars = text.value.length
+		const words = text.value.split(/\s+/).filter(word => word.length > 0).length
+
+		if (!words) return
+
+		checkMaxWords(words)
+
+		if (text.classList.contains('exceeded')) {
+			count.textContent = `You have exceeded the maximum number of ${maxWords} words.`
+		} else {
+			count.textContent = `You've written ${words} words and ${chars} characters.`
+		}
+	}
+
+	// Keep character count on refresh
+	updateCount()
+
+	text.addEventListener('input', updateCount, false)
+})()
